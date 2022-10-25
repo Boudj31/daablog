ff29ec3b5c0344dcc20210c43d3a39d8a95ac60e
Admin stagiaire <rboudjenane@jehann.fr>
2022-10-25 17:04:23 +0200

    installation de la l'application + premier composant Footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-indigo-500 text-center">DABLOG</h1>
+
+    <!-- MENU -->
+
+    <!--INTRODUCTION-->
+
+    <!--STATS -->
+
+    <!--DERNIERS POSTS -->
+
+    <!--FOOTER -->
+    <Footer />    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Footer
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
